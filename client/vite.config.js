3ee0ec9926020b5b20311b254370d870fca3ffdb
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-03-25 11:40:37 +0800

    Formatting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"),
+        },
     },
-  },
 });
