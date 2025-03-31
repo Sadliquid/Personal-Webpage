9e43bd1a08754cc3548e08b09b853605c0816e9e
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-03-31 12:05:47 +0800

    Added pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from './components/ui/provider'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import Portfolio from './pages/Portfolio'
+import Testimonial from './pages/Testimonial'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 import Layout from './Layout'
 
@@ -11,9 +16,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Homepage />} />
-				</Route>
+					<Route path="portfolio" element={<Portfolio />} />
+					<Route path="testimonial" element={<Testimonial />} />
+					<Route path="skills" element={<Skills />} />
+					<Route path="projects" element={<Projects />} />
+					<Route path="contact" element={<Contact />} />
 
-				<Route path="*" element={<NotFound />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	</Provider>
